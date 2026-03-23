3abd04c7e1aa8631e2a4c944175f67bfb7ad05a4
fcolombara <franciscocolombara@gmail.com>
2026-03-23 20:54:01 -0300

    Fix: Actualizada URL de la API de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://l4ga-backend.azurewebsites.net/api'
+  apiUrl: 'https://plan-gratuito-l4ga-fva3ave9athxdhfn.brazilsouth-01.azurewebsites.net/api'
 };
